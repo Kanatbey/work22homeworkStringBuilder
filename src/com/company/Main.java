63ff9ec6a6c2af8545400c5afd08ba4bdb625b89
Kanatbey <98470964+Kanatbey@users.noreply.github.com>
2022-03-04 22:52:11 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try(FileWriter fileWriter = new FileWriter("Alphabet.txt");) {
+            fileWriter.write("A a\n");
+            fileWriter.write("B b\n");
+            fileWriter.write("C c\n");
+            fileWriter.write("D d\n");
+            fileWriter.write("E e\n");
+            fileWriter.write("F f\n");
+            fileWriter.write("G g\n");
+            fileWriter.write("H h\n");
+            fileWriter.write("I i\n");
+            fileWriter.write("J j\n");
+            fileWriter.write("K k\n");
+            fileWriter.write("L l\n");
+            fileWriter.write("M m\n");
+            fileWriter.write("N n\n");
+            fileWriter.write("O o\n");
+            fileWriter.write("P p\n");
+            fileWriter.write("Q q\n");
+            fileWriter.write("R r\n");
+            fileWriter.write("S s\n");
+            fileWriter.write("T t\n");
+            fileWriter.write("U u\n");
+            fileWriter.write("V v\n");
+            fileWriter.write("W w\n");
+            fileWriter.write("X x\n");
+            fileWriter.write("Y y\n");
+            fileWriter.write("Z z\n");
+            fileWriter.write("0\n");
+            fileWriter.write("1\n");
+            fileWriter.write("2\n");
+            fileWriter.write("3\n");
+            fileWriter.write("4\n");
+            fileWriter.write("5\n");
+            fileWriter.write("6\n");
+            fileWriter.write("7\n");
+            fileWriter.write("8\n");
+            fileWriter.write("9\n");
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        try(FileReader fileReader = new FileReader("Alphabet.txt")) {
+            Scanner scanner = new Scanner(fileReader);
+            while (scanner.hasNextLine()){
+                for (int i = 1; i < 37; i++) {
+                    System.out.println(i + ": " + scanner.nextLine() );
+                }
+            }
+        }catch (IOException e){
+
+            System.out.println(e.getMessage());
+
+        }
+    }
+}
